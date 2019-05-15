610b49d49960f6e973a4284e9ddf321ebb063f4f
Alola-Kirby <1796650517@qq.com>
2019-05-15 17:04:06 +0800

    确定·公共部分·1

--- DBClass.py
+++ DBClass.py
@@ -1,28 +1,13 @@
 import Config
 from pymongo import MongoClient
+
 class DbOperate():
     '''
     连接数据库
     '''
     def __init__(self):
-        self.host=Config.HOST
-        self.port=Config.POST
-        self.client=MongoClient(self.host,self.post)
+        self.host = Config.HOST
+        self.port = Config.PORT
+        self.client = MongoClient(self.host, self.port)
 
-    '''
-    获取database
-    '''
-    def getcollection(self,database,collection):
-        self.database=self.client[database]
-        self.collection=self.database[collection]
-        return self.collection
-
-    '''
-    登录函数，需要连接
-    '''
-    def login(self):
-        self.client.
-
-if __name__ == '__main__':
-    db=DbOperate()
-    print(db.host)
+    
