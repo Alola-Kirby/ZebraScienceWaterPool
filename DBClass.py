0730ef3d7a004d21cf539f19aef7fe889ac8f770
xuzhida <1459601448@qq.com>
2019-05-16 21:11:50 +0800

    增加第十一个判断收藏该作品方法

--- DBClass.py
+++ DBClass.py
@@ -1,28 +1,36 @@
 import Config
 from pymongo import MongoClient
-class DbOperate():
+from json import dumps
+class DbOperate:
     '''
     连接数据库
     '''
     def __init__(self):
-        self.host=Config.HOST
-        self.port=Config.POST
-        self.client=MongoClient(self.host,self.post)
+        self.host = Config.HOST
+        self.port = Config.PORT
+        self.client = MongoClient(self.host, self.port)
+
 
     '''
-    获取database
+    the 10th Method
+    收藏/取消收藏资源，根据paperid是否在收藏列表中来判断是收藏还是取消收藏
+    方案存疑，待解决
     '''
-    def getcollection(self,database,collection):
-        self.database=self.client[database]
-        self.collection=self.database[collection]
-        return self.collection
+    def collect(self,userid,paperid):
+        pass
+
 
     '''
-    登录函数，需要连接
+    The 11th Method
+    判断用户是否收藏该作品
     '''
-    def login(self):
-        self.client.
+    def is_collect(self,userid,paperid):
+        user_collection = self.client.Business.user
+        user= user_collection.find_one({'userid': userid})
+        if paperid not in user.star_list:
+            state={'state':'no','reasons':'用户尚未收藏该资源'}
+        else:
+            state={'state':'yes','reasons':'用户已收藏该资源'}
+        return dumps(state)
 
-if __name__ == '__main__':
-    db=DbOperate()
-    print(db.host)
+    
