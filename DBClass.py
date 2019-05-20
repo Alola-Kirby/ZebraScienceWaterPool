6ef3740bbfdb17661943412cae514e06f24780bf
Alola-Kirby <1796650517@qq.com>
2019-05-20 20:35:55 +0800

    接口1-6
    rt

--- DBClass.py
+++ DBClass.py
@@ -1,3 +1,4 @@
+import copy
 import re
 import Config
 import random
@@ -76,7 +77,7 @@ class DbOperate:
     '''
     2. 注册用户 √
     '''
-    def create_user(self, password, email, username, avatar, email_code):
+    def create_user(self, password, email, username, email_code):
         res = {'state': 'fail', 'reason': '网络出错或BUG出现！'}
         try:
             has_user = self.getCol('user').find({'email': email}).count()
@@ -89,7 +90,6 @@ class DbOperate:
                 newuser = { 'username': username,
                             'email': email,
                             'password': password,
-                            'avatar': avatar,
                             'user_type': 'USER',
                             'star_list': [],
                             'follow_list': []
@@ -192,7 +192,7 @@ class DbOperate:
             return res
 
     '''
-    6. 获取用户信息
+    6. 获取用户信息 √
     '''
     def get_user_details(self, email):
         res = {'state': 'fail', 'reason': '网络出错或BUG出现！'}
@@ -200,9 +200,36 @@ class DbOperate:
             find_user = self.getCol('user').find_one({'email': email})
             # 搜索到指定用户
             if find_user:
+                # 去掉某些不必要字段
                 find_user.pop('_id')
                 find_user.pop('password')
-                find_user.pop('user_type')
+                # 将star_list和follow_list中的id和简略信息一并返回
+                # 令star_list列表中存放“资源的简略信息”
+                tmp_star = copy.deepcopy(find_user['star_list'])
+                find_user['star_list'].clear()
+                res['reason'] = '收藏列表获取失败'
+                for one_star in tmp_star:
+                    star_info = self.getCol('sci_source').find_one({'paperid': one_star})
+                    star_info.pop('_id')
+                    star_info.pop('source_url')
+                    star_info.pop('free_download_url')
+                    star_info.pop('abstract')
+                    find_user['star_list'].append(star_info)
+                # 令follow_list列表中存放“用户的简略信息”
+                tmp_follow = copy.deepcopy(find_user['follow_list'])
+                find_user['follow_list'].clear()
+                res['reason'] = '关注列表获取失败'
+                for one_follow in tmp_follow:
+                    follow_info_all = self.getCol('scmessage').find_one({'scid': one_follow})
+                    follow_info_simple = {}
+                    follow_info_simple['scid'] = one_follow
+                    follow_info_simple['name'] = follow_info_all['name']
+                    follow_info_simple['mechanism'] = follow_info_all['mechanism']
+                    follow_info_simple['citedtimes'] = follow_info_all['citedtimes']
+                    follow_info_simple['resultsnumber'] = follow_info_all['resultsnumber']
+                    follow_info_simple['field'] = follow_info_all['field']
+                    find_user['follow_list'].append(follow_info_simple)
+                # 设置返回值
                 res['state'] = 'success'
                 res['msg'] = find_user
             # 用户不存在
